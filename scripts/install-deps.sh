182f15b0813f7666b4002385e0cc50bf7d6bac5e
ToropovAlexei <henschel37rus@gmail.com>
2024-11-03 18:18:41 +0300

    dotfiles updated

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
 yay -S --noconfirm --needed hyprland \
     nerd-fonts noto-fonts \
     nvidia nvidia-utils egl-wayland \
-    wofi waybar kitty \
+    rofi-lbonn-wayland-git waybar kitty \
     firefox google-chrome discord \
     zed visual-studio-code-bin \
     grim slurp wl-clipboard \
@@ -16,7 +22,7 @@ yay -S --noconfirm --needed hyprland \
     zsh zsh-theme-powerlevel10k-git \
     telegram-desktop python-pip python-requests net-tools \
     sshpass wlogout swappy grimblast \
-    gimp imv archlinux-xdg-menu btop
+    gimp imv archlinux-xdg-menu btop tracy
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
